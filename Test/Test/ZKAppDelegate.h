fc2492a96e55a94793a9b59795146c3758e158e1
Zeeshan Khan <zeeshan.s.khan@gmail.com>
2012-12-30 06:33:20 -0800

    Test

---
+++ Test/Test/ZKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZKAppDelegate.h
+//  Test
+//
+//  Created by Zeeshan Khan on 30/12/12.
+//  Copyright (c) 2012 Zeeshan Khan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
